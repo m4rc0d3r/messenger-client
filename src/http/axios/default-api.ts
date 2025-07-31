a49396a0887240ff85b7ead05aac2ac3cabc2f4a
Mark <m4rc0d3r@gmail.com>
2025-07-31 16:52:59 +0300

    Add base url to default axios api in request interceptor

--- src/http/axios/default-api.ts
+++ src/http/axios/default-api.ts
@@ -4,11 +4,10 @@ import { useConfigStore } from "@/config";
 import { useAuthStore } from "@/stores/auth-store";
 import { pinia } from "@/stores/pinia";
 
-export const defaultAPI = axios.create({
-  baseURL: useConfigStore(pinia).config.serverApp.httpUrl,
-});
+export const defaultAPI = axios.create();
 
 defaultAPI.interceptors.request.use((config) => {
+  config.baseURL = useConfigStore(pinia).config.serverApp.httpUrl;
   if (config.headers !== undefined) {
     config.headers.authorization = `Bearer ${useAuthStore(pinia).token.value}`;
   }
